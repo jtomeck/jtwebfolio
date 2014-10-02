581ebeea12ecf465cd47232c8fe90ac0e2566003
Jared Tomeck <jared.tomeck@gmail.com>
2014-10-02 01:40:58 -0400

    adding post_thumbnail support

--- wp-content/themes/jtwebfolio/content-page.php
+++ wp-content/themes/jtwebfolio/content-page.php
@@ -4,24 +4,25 @@
  *
  * @package jtwebfolio
  */
+
+//Featured Image
+$thumb_id = get_post_thumbnail_id( $post->ID );
+
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
+		<?php if ( $thumb_id ) : ?>
+            <div class="entry-image">
+                <?php $thumb_url_array = wp_get_attachment_image_src($thumb_id, 'full', true); ?>
+                <img class="entry-featured-image" src="<?php echo $thumb_url_array[0]; ?>" alt="Banner Text">
+            </div><!-- .entry-image -->
+        <?php endif; ?>
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
 		<?php the_content(); ?>
-		<?php
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . __( 'Pages:', 'jtwebfolio' ),
-				'after'  => '</div>',
-			) );
-		?>
 	</div><!-- .entry-content -->
 
-	<footer class="entry-footer">
-		<?php edit_post_link( __( 'Edit', 'jtwebfolio' ), '<span class="edit-link">', '</span>' ); ?>
-	</footer><!-- .entry-footer -->
 </article><!-- #post-## -->
