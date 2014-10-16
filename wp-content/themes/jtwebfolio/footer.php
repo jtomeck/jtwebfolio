6091668fabfca3c4b3d2f13f2194528a7ee9a0f6
Jared Tomeck <jared.tomeck@gmail.com>
2014-10-15 22:28:13 -0400

    google analytics code

--- wp-content/themes/jtwebfolio/footer.php
+++ wp-content/themes/jtwebfolio/footer.php
@@ -47,6 +47,15 @@ $menu_toggle = $cfs->get('jtw_toggle_menu');
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
+  ga('create', 'UA-55803910-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
 </body>
 </html>
